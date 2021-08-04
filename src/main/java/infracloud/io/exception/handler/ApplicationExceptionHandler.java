f1199b27c9172aad9e0d23ac17a0b9ffee335b6b
admin <admin@DESKTOP-BC7IPPU.mshome.net>
2021-08-04 17:11:25 +0530

    refactored exception handling

--- src/main/java/infracloud/io/exception/handler/ApplicationExceptionHandler.java
+++ src/main/java/infracloud/io/exception/handler/ApplicationExceptionHandler.java
@@ -1,5 +1,9 @@
 package infracloud.io.exception.handler;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +12,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import infracloud.io.controller.UrlController;
+
 @ControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+	private static final Logger LOGGER = LoggerFactory.getLogger(UrlController.class);
+	
 	@ExceptionHandler(value = { Exception.class })
 	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+		LOGGER.error(Arrays.toString(ex.getStackTrace()));
+		
 		String bodyOfResponse = "url-service exception: "+ex.getMessage();
 		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
